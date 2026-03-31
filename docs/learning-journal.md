15532ee83cce5c73343e0625bf51453fb9d31174
VladPetac <vladalexandru2012@yahoo.com>
2026-03-31 21:18:45 +0300

    Add README.md file in each env directory, add log output in setup.log and on screen, add --dry-run flag in setup-project.sh, add learning-journal.md

---
+++ docs/learning-journal.md
@@ -0,0 +1,7 @@
+# ====================
+# = Learning Journal =
+# =  Weekly review   =
+# ====================
+
+# Week 1 (Phase 0, 1, 2 ,3)
+# Managed to easily navigate through the Terminal. Also, learning and understanding better and better every day how GIT works.
--- scripts/setup-project.sh
+++ scripts/setup-project.sh
@@ -18,14 +18,16 @@ ENV_NAME="${1:-dev}"                            # First argument, default "dev" 
 
 # --- Functions ---
 
-# Log function: prints timestamped messages
+LOG_FILE="logs/setup.log"
+
+# Log function: prints timestamped messages on the screen and also in the setup.log file
 log() {
-  echo "[$TIMESTAMP] [INFO] $1"
+  echo "[$TIMESTAMP] [INFO] $1" | tee -a "$LOG_FILE"
 }
 
-# Error function: prints errors and exits
+# Error function: prints errors and exits on the screen and also in the setup.log file
 error() {
-  echo "[$TIMESTAMP] [ERROR] $1" >&2            # >&2 sends output to stderr (error stream)
+  echo "[$TIMESTAMP] [ERROR] $1" | tee -a "$LOG_FILE" >&2            # >&2 sends output to stderr (error stream)
   exit 1
 }
 
@@ -48,7 +50,11 @@ DIRS=("configs/$ENV_NAME" "logs/$ENV_NAME" "backups/$ENV_NAME")
 
 for dir in "${DIRS[@]}"; do
   if [ -d "$dir" ]; then
-    log "Directory already exists: $dir"
+    if [ "$DRY_RUN" = true ]; then
+      log "[DRY RUN] Would create directory: $dir"
+    else
+      log "Directory already exists: $dir"
+    fi
   else
     mkdir -p "$dir"
     log "Created directory: $dir"
@@ -58,21 +64,41 @@ done
 # Create environment config file
 CONFIG_FILE="configs/$ENV_NAME/environment.yaml"
 if [ ! -f "$CONFIG_FILE" ]; then
-  cat << EOF > "$CONFIG_FILE"
-# MicroBank Configuration - $ENV_NAME environment
-# Generated on: $TIMESTAMP
+  if [ "$DRY_RUN" = true ]; then
+    log "[DRY RUN] Would create config file: $CONFIG_FILE"
+  else
+cat << EOF > "$CONFIG_FILE"
+      # MicroBank Configuration - $ENV_NAME environment
+      # Generated on: $TIMESTAMP
 
-environment: $ENV_NAME
-debug_mode: $([ "$ENV_NAME" = "dev" ] && echo "true" || echo "false")
+      environment: $ENV_NAME
+      debug_mode: $([ "$ENV_NAME" = "dev" ] && echo "true" || echo "false")
 
-database:
-  host: postgres-$ENV_NAME
-  port: 5432
-  name: microbank_$ENV_NAME
+      database:
+        host: postgres-$ENV_NAME
+        port: 5432
+        name: microbank_$ENV_NAME
 EOF
   log "Created config file: $CONFIG_FILE"
+  fi
 else
   log "Config file already exists: $CONFIG_FILE"
 fi
+
+# Create environment README file
+README_FILE="configs/$ENV_NAME/README.md"
+if [ ! -f "$README_FILE" ]; then
+  if [ "$DRY_RUN" = true ]; then
+    log "[DRY RUN] Would create README file: $README_FILE"
+  else
+cat << EOF > "$README_FILE"
+      # MicroBank Readme - $ENV_NAME readme file
+      # Generated on: $TIMESTAMP
+EOF
+    log "Created readme file: $README_FILE"
+  fi
+  else
+    log "README file already exists: $README_FILE"
+  fi
 
 log "Setup complete for $ENV_NAME environment!"
